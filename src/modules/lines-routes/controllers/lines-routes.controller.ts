cc8de1469cd9322e02e4f00495fabc7e849a539c
Arnol Guevara <arnolpacman@gmail.com>
2023-08-17 10:20:43 -0400

    updated

--- src/modules/lines-routes/controllers/lines-routes.controller.ts
+++ src/modules/lines-routes/controllers/lines-routes.controller.ts
@@ -1,27 +1,33 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
-import { LinesRoutesService } from "@modules/lines-routes/services/lines-routes.service";
-import { LineString } from "geojson";
-import { CompareLinestringsDto } from "@modules/lines-routes/models/dto/compare-linestrings.dto";
+import {Body, Controller, Get, Param, Post} from "@nestjs/common";
+import {LinesRoutesService} from "@modules/lines-routes/services/lines-routes.service";
+import {LineString} from "geojson";
+import {CompareLinestringsDto} from "@modules/lines-routes/models/dto/compare-linestrings.dto";
+import {NearestLinesRoutesDto} from "@modules/lines-routes/models/dto/nearest-lines-routes.dto";
 
 @Controller("lines-routes")
 export class LinesRoutesController {
-  constructor(
-    private readonly linesRoutesService: LinesRoutesService
-  ) {
-  }
+    constructor(
+        private readonly linesRoutesService: LinesRoutesService
+    ) {
+    }
 
-  @Get()
-  async findAll() {
-    return await this.linesRoutesService.findAll();
-  }
+    @Get()
+    async findAll() {
+        return await this.linesRoutesService.findAll();
+    }
 
-  @Get("/:id")
-  async findOne(@Param("id") id: number) {
-    return await this.linesRoutesService.findOne(+id);
-  }
+    @Get("/:id")
+    async findOne(@Param("id") id: number) {
+        return await this.linesRoutesService.findOne(+id);
+    }
 
-  @Get("/find-by-name/:name")
-  async findByName(@Param("name") name: string) {
-    return await this.linesRoutesService.findByName(name);
-  }
+    @Get("/find-by-name/:name")
+    async findByName(@Param("name") name: string) {
+        return await this.linesRoutesService.findByName(name);
+    }
+
+    @Post("/find-nearest-lines-routes")
+    async findNearestLinesRoutes(@Body() nearestLinesRoutesDto: NearestLinesRoutesDto) {
+        return await this.linesRoutesService.findNearestLinesRoutes(nearestLinesRoutesDto);
+    }
 }
--- src/modules/lines-routes/services/lines-routes.service.ts
+++ src/modules/lines-routes/services/lines-routes.service.ts
@@ -1,30 +1,46 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { LineRouteEntity } from "@modules/lines-routes/models/entities/line-route.entity";
-import { Repository } from "typeorm";
+import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {LineRouteEntity} from "@modules/lines-routes/models/entities/line-route.entity";
+import {Repository} from "typeorm";
+import {NearestLinesRoutesDto} from "@modules/lines-routes/models/dto/nearest-lines-routes.dto";
 
 @Injectable()
 export class LinesRoutesService {
 
-  constructor(
-    @InjectRepository(LineRouteEntity)
-    private readonly lineRouteRepository: Repository<LineRouteEntity>
-  ) {
-  }
+    constructor(
+        @InjectRepository(LineRouteEntity)
+        private readonly lineRouteRepository: Repository<LineRouteEntity>
+    ) {
+    }
 
-  async findAll() {
-    return await this.lineRouteRepository.find();
-  }
+    async findAll() {
+        return await this.lineRouteRepository.find();
+    }
 
-  async findOne(id: number) {
-    return await this.lineRouteRepository.findOne({
-      where: { id }
-    });
-  }
+    async findOne(id: number) {
+        return await this.lineRouteRepository.findOne({
+            where: {id}
+        });
+    }
 
-  async findByName(name: string) {
-    return await this.lineRouteRepository.find({
-      where: { name }
-    });
-  }
+    async findByName(name: string) {
+        return await this.lineRouteRepository.find({
+            where: {name}
+        });
+    }
+
+    async findNearestLinesRoutes(nearestLinesRoutesDto: NearestLinesRoutesDto) {
+        const {coordinate} = nearestLinesRoutesDto;
+        return await this.lineRouteRepository.query(`
+            SELECT name
+             FROM lines_names 
+             WHERE name IN (SELECT name FROM (SELECT *, ST_DistanceSphere(
+            geom,
+            ST_GeomFromText('POINT(${coordinate[0]} ${coordinate[1]})', 4326)
+            ) AS distance
+            FROM lines_routes
+            ORDER BY distance ASC
+            LIMIT 8) AS nearest_lines_routes)
+        `);
+    }
 }
