17fa81561688c395a1b142d57f3a14bd93203a37
Arnol Guevara <arnolpacman@gmail.com>
2023-08-18 10:22:10 -0400

    updated

--- src/modules/lines-routes/services/lines-routes.service.ts
+++ src/modules/lines-routes/services/lines-routes.service.ts
@@ -48,7 +48,7 @@ export class LinesRoutesService {
                     lr.name
                 ORDER BY 
                     min_distance ASC
-                LIMIT 16;
+                LIMIT 20;
             `);
         }
         return names;
