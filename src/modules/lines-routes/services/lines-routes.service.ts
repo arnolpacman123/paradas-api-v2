25823aca10b4f64f25ed2e042dca61315cebfa80
Arnol Guevara <arnolpacman@gmail.com>
2023-08-18 09:18:40 -0400

    updated

--- src/modules/lines-routes/services/lines-routes.service.ts
+++ src/modules/lines-routes/services/lines-routes.service.ts
@@ -1,8 +1,8 @@
-import {Injectable} from "@nestjs/common";
-import {InjectRepository} from "@nestjs/typeorm";
-import {LineRouteEntity} from "@modules/lines-routes/models/entities/line-route.entity";
-import {Repository} from "typeorm";
-import {NearestLinesRoutesDto} from "@modules/lines-routes/models/dto/nearest-lines-routes.dto";
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { LineRouteEntity } from "@modules/lines-routes/models/entities/line-route.entity";
+import { Repository } from "typeorm";
+import { NearestLinesRoutesDto } from "@modules/lines-routes/models/dto/nearest-lines-routes.dto";
 
 @Injectable()
 export class LinesRoutesService {
@@ -19,25 +19,24 @@ export class LinesRoutesService {
 
     async findOne(id: number) {
         return await this.lineRouteRepository.findOne({
-            where: {id}
+            where: { id }
         });
     }
 
     async findByName(name: string) {
         return await this.lineRouteRepository.find({
-            where: {name}
+            where: { name }
         });
     }
 
     async findNearestLinesRoutes(nearestLinesRoutesDto: NearestLinesRoutesDto) {
-        const {coordinate} = nearestLinesRoutesDto;
+        const { coordinate } = nearestLinesRoutesDto;
+        // Encontrar el nombre de las líneas (una sola vez una línea) que estén a menos de 400 metros de la ubicación y ordenarlas por distancia
         return await this.lineRouteRepository.query(`
-            SELECT distinct(name)
-            FROM lines_routes
-            WHERE ST_DistanceSphere(
-                geom,
-                ST_MakePoint(${coordinate[0]}, ${coordinate[1]})
-            ) <= 400
+            SELECT DISTINCT (lr.name), ST_DistanceSphere(lr.geom, ST_SetSRID(ST_MakePoint(${coordinate[0]}, ${coordinate[1]}), 4326)) AS distance
+            FROM lines_routes lr
+            WHERE ST_DWithin(lr.geom, ST_SetSRID(ST_MakePoint(${coordinate[0]}, ${coordinate[1]}), 4326), 0.002)
+            ORDER BY distance ASC;
         `);
     }
 }
