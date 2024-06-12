c951e79fb45336f019c152b84a2a8edcd453618b
Arnol Guevara <arnolpacman@gmail.com>
2024-06-12 10:29:55 -0400

    updated

--- src/modules/lines-routes/services/lines-routes.service.ts
+++ src/modules/lines-routes/services/lines-routes.service.ts
@@ -1,66 +1,90 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { LineRouteEntity } from "@modules/lines-routes/models/entities/line-route.entity";
-import { Repository } from "typeorm";
-import { NearestLinesRoutesDto } from "@modules/lines-routes/models/dto/nearest-lines-routes.dto";
-import { FindLineRouteDto } from "@modules/lines-routes/models/dto/find-line-route.dto";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LineRouteEntity } from '@modules/lines-routes/models/entities/line-route.entity';
+import { Repository } from 'typeorm';
+import { NearestLinesRoutesDto } from '@modules/lines-routes/models/dto/nearest-lines-routes.dto';
+import { FindLineRouteDto } from '@modules/lines-routes/models/dto/find-line-route.dto';
 
 @Injectable()
 export class LinesRoutesService {
+  constructor(
+    @InjectRepository(LineRouteEntity)
+    private readonly lineRouteRepository: Repository<LineRouteEntity>,
+  ) {}
 
-    constructor(
-        @InjectRepository(LineRouteEntity)
-        private readonly lineRouteRepository: Repository<LineRouteEntity>
-    ) {
-    }
+  async findAll() {
+    return await this.lineRouteRepository.find();
+  }
 
-    async findAll() {
-        return await this.lineRouteRepository.find();
-    }
+  async findOne(id: number) {
+    return await this.lineRouteRepository.findOne({
+      where: { id },
+    });
+  }
 
-    async findOne(id: number) {
-        return await this.lineRouteRepository.findOne({
-            where: { id }
-        });
-    }
+  async findByName(name: string) {
+    return await this.lineRouteRepository.find({
+      where: { name },
+    });
+  }
 
-    async findByName(name: string) {
-        return await this.lineRouteRepository.find({
-            where: { name }
-        });
-    }
-
-    async findNearestLinesRoutes(nearestLinesRoutesDto: NearestLinesRoutesDto) {
-        const { coordinate } = nearestLinesRoutesDto;
-        let names = await this.lineRouteRepository.query(`
-            SELECT lr.name, 
-            MIN(ST_DistanceSphere(lr.geom, ST_SetSRID(ST_MakePoint(${coordinate[0]}, ${coordinate[1]}), 4326))) AS distance
-            FROM lines_routes lr
-            WHERE ST_DWithin(lr.geom, ST_SetSRID(ST_MakePoint(${coordinate[0]}, ${coordinate[1]}), 4326), 0.001)
-            GROUP BY lr.name
-            ORDER BY distance ASC;
+  async findNearestLinesRoutes(nearestLinesRoutesDto: NearestLinesRoutesDto) {
+    const { coordinate } = nearestLinesRoutesDto;
+    let names = await this.lineRouteRepository.query(`
+            WITH ordered_routes AS (
+                SELECT lr.name
+                        FROM (
+                    SELECT lr.name, 
+                    MIN(ST_DistanceSphere(lr.geom, ST_SetSRID(ST_MakePoint(${coordinate[0]}, ${coordinate[1]}), 4326))) AS distance
+                    FROM lines_routes lr
+                    WHERE ST_DWithin(lr.geom, ST_SetSRID(ST_MakePoint(${coordinate[0]}, ${coordinate[1]}), 4326), 0.001)
+                    GROUP BY lr.name
+                    ORDER BY distance ASC
+                ) AS lr
+            )
+            SELECT ln.*
+            FROM lines_names ln
+            JOIN ordered_routes ordr ON ln.name = ordr.name
+            ORDER BY array_position(array(SELECT name FROM ordered_routes), ln.name);
         `);
-        if (names.length === 0) {
-            names = await this.lineRouteRepository.query(`
-                SELECT 
-                    lr.name, 
-                    MIN(ST_DistanceSphere(lr.geom, ST_SetSRID(ST_MakePoint(${coordinate[0]}, ${coordinate[1]}), 4326))) AS min_distance
-                FROM 
-                    lines_routes lr
-                GROUP BY 
-                    lr.name
-                ORDER BY 
-                    min_distance ASC
-                LIMIT 18;
-            `);
-        }
-        return names;
-    }
-
-    async findLineRoute(findLineRouteDto: FindLineRouteDto) {
-        const { name, ground } = findLineRouteDto;
-        return await this.lineRouteRepository.findOne({
-            where: { name, ground }
+    if (names.length === 0) {
+      names = await this.lineRouteRepository
+        .query(
+          `
+                WITH ordered_routes AS (
+                    SELECT lr.name
+                    FROM (
+                        SELECT lr.name,
+                            MIN(ST_DistanceSphere(lr.geom, ST_SetSRID(ST_MakePoint(${coordinate[0]}, ${coordinate[1]}), 4326))) AS min_distance
+                        FROM lines_routes lr
+                        GROUP BY lr.name
+                        ORDER BY min_distance ASC
+                        LIMIT 18
+                    ) AS lr
+                )
+                SELECT ln.*
+                FROM lines_names ln
+                JOIN ordered_routes ordr ON ln.name = ordr.name
+                ORDER BY array_position(array(SELECT name FROM ordered_routes), ln.name);
+            `,
+        )
+        .then((res) => {
+          return res.map((item: Object) => {
+            return {
+              id: item['id'],
+              name: item['name'],
+              imageUrl: item['image_url'],
+            };
+          });
         });
     }
+    return names;
+  }
+
+  async findLineRoute(findLineRouteDto: FindLineRouteDto) {
+    const { name, ground } = findLineRouteDto;
+    return await this.lineRouteRepository.findOne({
+      where: { name, ground },
+    });
+  }
 }
--- src/modules/stops-points/models/entities/stop-point.entity.ts
+++ src/modules/stops-points/models/entities/stop-point.entity.ts
@@ -1,6 +1,5 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { Point } from "geojson";
-import { ChannelRouteEntity } from "@modules/channels-routes/models/entities/channel-route.entity";
 
 @Entity({
     name: 'stops_points'
@@ -15,8 +14,4 @@ export class StopPointEntity {
         srid: 4326
     })
     geom: Point;
-
-    // @ManyToOne(() => ChannelRouteEntity, channel_route => channel_route.stops_points)
-    // @JoinColumn({ name: 'channel_route_id' })
-    // channel_route: ChannelRouteEntity;
 }
